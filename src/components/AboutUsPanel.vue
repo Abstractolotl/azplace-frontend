63da4958f7689a817b163b9f17addd3290fb86af
Lucas Pape <lpape@proton.me>
2022-08-19 16:19:57 +0200

    Update image api url

--- src/components/AboutUsPanel.vue
+++ src/components/AboutUsPanel.vue
@@ -4,14 +4,14 @@
         If you want to know more about the creation process don't be afraid to get in contact with us 
         
         <div>
-            <a href="https://united-internet.org/profiles/people/21550081"><img src="https://image.azubi.server.lan/picture/21550081"/></a>
-            <a href="https://united-internet.org/profiles/people/21549706"><img src="https://image.azubi.server.lan/picture/21549706"/></a>
-            <a href="https://united-internet.org/profiles/people/21566957"><img src="https://image.azubi.server.lan/picture/21566957"/></a>
-            <a href="https://united-internet.org/profiles/people/21549699"><img src="https://image.azubi.server.lan/picture/21549699"/></a>
-            <a href="https://united-internet.org/profiles/people/21549701"><img src="https://image.azubi.server.lan/picture/21549701"/></a>
-            <a href="https://united-internet.org/profiles/people/21549705"><img src="https://image.azubi.server.lan/picture/21549705"/></a>
-            <a href="https://united-internet.org/profiles/people/21549708"><img src="https://image.azubi.server.lan/picture/21549708"/></a>
-            <a href="https://united-internet.org/profiles/people/21549700"><img src="https://image.azubi.server.lan/picture/21549700"/></a>
+            <a href="https://united-internet.org/profiles/people/21550081"><img src="https://api.azubi.server.lan/image/personalpicture/21550081"/></a>
+            <a href="https://united-internet.org/profiles/people/21549706"><img src="https://api.azubi.server.lan/image/personalpicture/21549706"/></a>
+            <a href="https://united-internet.org/profiles/people/21566957"><img src="https://api.azubi.server.lan/image/personalpicture/21566957"/></a>
+            <a href="https://united-internet.org/profiles/people/21549699"><img src="https://api.azubi.server.lan/image/personalpicture/21549699"/></a>
+            <a href="https://united-internet.org/profiles/people/21549701"><img src="https://api.azubi.server.lan/image/personalpicture/21549701"/></a>
+            <a href="https://united-internet.org/profiles/people/21549705"><img src="https://api.azubi.server.lan/image/personalpicture/21549705"/></a>
+            <a href="https://united-internet.org/profiles/people/21549708"><img src="https://api.azubi.server.lan/image/personalpicture/21549708"/></a>
+            <a href="https://united-internet.org/profiles/people/21549700"><img src="https://api.azubi.server.lan/image/personalpicture/21549700"/></a>
         </div>
     </div>
 </template>
