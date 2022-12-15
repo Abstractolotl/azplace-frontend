b5a6228892368e184f6c06b6cfcf574c75a70e1c
Ancocodet <ancoplays@gmail.com>
2022-12-15 13:40:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: "./config",
+  envDir: "/config",
   plugins: [vue()],
   resolve: {
     alias: {
